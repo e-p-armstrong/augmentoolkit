00f35fe4a4f659027a6b20c6abb03b6769d627b1
cocktailpeanut <cocktailpeanuts@proton.me>
2024-05-16 06:06:22 -0400

    update

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import gradio as gr
 import yaml
 
+
 with open('config.yaml', 'r') as file:
   config = yaml.safe_load(file)
-
+  str = yaml.dump(config)
 
 def greet(name, intensity):
     return "Hello, " + name + "!" * int(intensity)
 
-textbox = gr.Textbox(value=file)
+textbox = gr.Textbox(value=str)
 demo = gr.Interface(
     fn=greet,
     inputs=[textbox],
