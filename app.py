becfaf68041fced7ee592bdbea88e37dd0badee8
cocktailpeanut <cocktailpeanuts@proton.me>
2024-05-16 08:09:52 -0400

    gradio

--- app.py
+++ app.py
@@ -1,19 +1,49 @@
 import gradio as gr
 import yaml
+import subprocess
+from gradio_log import Log
 
-
+config = {}
 with open('config.yaml', 'r') as file:
   config = yaml.safe_load(file)
   str = yaml.dump(config)
+  components = []
+  for key1 in config:
+    group = []
+    for key2 in config[key1]:
+      group.append({'path': [key1, key2], 'label':key2, 'value':config[key1][key2]})
+    components.append(group)
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
+def changed(text, keys):
+  global config
+  _config = config
+  for key in keys[:-1]:
+    _config = _config.setdefault(key, {})
+  _config[keys[-1]] = text
+  return keys
 
-textbox = gr.Textbox(value=str)
-demo = gr.Interface(
-    fn=greet,
-    inputs=[textbox],
-    outputs=["text"],
-)
+def run():
+  global config
+  with open('config.yaml', 'w') as file:
+    yaml.dump(config, file)
+  try:
+    with open("log.txt", "w") as log_file:
+      subprocess.run(["python", "processing.py"], stdout=log_file, text=True)
+  except subprocess.CalledProcessError as e:
+    print(f"Error: {e}")
 
+with gr.Blocks() as demo:
+  with gr.Row():
+    log_file = "log.txt"
+    Log(log_file, dark=True, xterm_font_size=12)
+    btn = gr.Button("Start")
+    btn.click(fn=run, inputs=[], outputs=[])
+  with gr.Row():
+    for component in components:
+      print(f"component = {component}")
+      with gr.Column():
+        for item in component:
+          print(f"item={item}")
+          t = gr.Textbox(label=item['label'], value=item['value'], interactive=True)
+          t.change(changed, [t, gr.State(value=item['path'])], [gr.State()])
 demo.launch()
