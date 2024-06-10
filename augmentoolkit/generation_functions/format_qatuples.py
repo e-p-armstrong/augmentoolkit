87599a3602016dcf4ac95256c4799adc94f0047b
Evan Armstrong <evanpeterarmstrong@gmail.com>
2024-06-10 16:48:25 -0700

    code revisions initial complete, and they work

--- augmentoolkit/generation_functions/format_qatuples.py
+++ augmentoolkit/generation_functions/format_qatuples.py
@@ -2,14 +2,11 @@ def format_qatuples(qatuples):
     strlst = []
     for qatuple in qatuples:
         strlst.append(
-            f"""Question: \"\"\"{qatuple[0]}\"\"\"
-Answer: \"\"\"{qatuple[1]}\"\"\""""
+            f"""**QUESTION:**
+{qatuple[0]}
+
+**ANSWER:**
+{qatuple[1]}
+"""
         )
     return "\n\n".join(strlst)
-
-
-def format_qatuples_noquotes(qatuples):
-    strlst = []
-    for idx, qatuple in enumerate(qatuples):
-        strlst.append(f"""{idx + 1}. {qatuple[0]}""")
-    return "\n".join(strlst)
